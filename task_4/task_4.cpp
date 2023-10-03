7e5151e58017a277e740bf80516232dec17a0e39
VadimS20 <vadim_2014_pushkin@mail.ru>
2023-10-03 17:41:56 +0300

    Добавьте файлы проекта.

---
+++ task_4/task_4.cpp
@@ -0,0 +1,18 @@
+﻿#include "task_4.h"
+
+using namespace std;
+
+
+int main()
+{
+	list<int> mylist;
+	mylist.addNode(1,0);
+	mylist.addNode(2,1);
+	mylist.addNode(3,2);
+	mylist.addNode(17, 3);
+	mylist.showNode();
+	cout << "\n";
+	mylist.removeNode(3);
+	mylist.showNode();
+	return 0;
+}
---
+++ task_4/task_4.h
@@ -0,0 +1,102 @@
+﻿#pragma once
+#include <optional>
+#include <iostream>
+using namespace std;
+
+template <typename typeData>
+class list {
+private:
+	struct Node {
+		Node* next;
+		typeData data;
+	};
+		Node* head;
+public:
+	list() {
+		head = nullptr;
+	};
+
+	list(list&&) {
+
+	}
+
+	~list() {
+		Node* temp= head;
+		while (temp->next != nullptr) {
+			Node* nextTemp = temp->next;
+			delete temp;
+			temp = nextTemp;
+		}
+	}
+
+	void addNode(typeData newdata,int pos) {
+		Node* last = head;
+		Node* temp=new Node;
+		temp->data = newdata;
+		temp->next = nullptr;
+		if (head == nullptr) {
+			head = temp;
+		}
+		else {
+			if (pos == 0) {
+				temp->next = temp;
+				head = temp;
+			}
+			else {
+				while (pos != 0 && (last->next != nullptr)) {
+					last=last->next;
+					pos--;
+				}
+				temp->next = nullptr;
+				last->next = temp;
+			}
+		}
+
+	};
+
+	void removeNode(int pos) {
+		Node* temp = head;
+		Node* prevTemp= new Node;
+		if (pos == 0 ) {
+			if (head->next) {
+				head = head->next;
+			}
+			else
+			{
+				head = nullptr;
+			}
+		}
+		else {
+			while (pos != 0 ) {
+				prevTemp = temp;
+				temp = temp->next;
+				pos--;
+			}
+			if (temp->next != nullptr) {
+				prevTemp->next = temp->next;
+			}
+			else {
+				prevTemp->next = nullptr;
+			}
+			delete temp;
+		}
+	}
+
+	std::optional<typeData> GetElement(int pos) {
+		Node* temp = head;
+		while (pos != 0) {
+			temp = temp->next;
+			pos--;
+		}
+		return temp->data;
+	}
+
+	void showNode() {
+		Node* point = head;
+		while (point != nullptr) {
+			cout << point->data << "\n";
+			point = point->next;
+		};
+
+	};
+};
